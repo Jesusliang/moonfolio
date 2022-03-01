6c09537ac3147ca6f3d8e79b7e9aaa269042b5ed
Jesus Liang <jesusliang96@gmail.com>
2022-03-01 23:14:26 +0100

    added error handling to email

--- components/contactMe/ContactMe.tsx
+++ components/contactMe/ContactMe.tsx
@@ -1,4 +1,4 @@
-import { CheckCircleIcon } from "@chakra-ui/icons";
+import { CheckCircleIcon, WarningIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -32,21 +32,147 @@ const ContactMe: React.FC<Props> = (props) => {
   }>();
   const [sendEmailLoading, setSendEmailLoading] = useState(false);
   const [emailSent, setEmailSent] = useState(false);
+  const [error, setError] = useState(false);
   const onSubmit = handleSubmit(async (data) => {
     setSendEmailLoading(true);
     try {
       const response = await axios.post("/api/mailer", data);
-      // await new Promise((resolve) => {
-      //   setTimeout(() => {
-      //     resolve("");
-      //   }, 2000);
-      // });
       setEmailSent(true);
     } catch (error) {
+      setError(true);
     } finally {
       setSendEmailLoading(false);
     }
   });
+
+  const renderEmailBox = () => {
+    if (error) {
+      return (
+        <Box
+          textAlign="center"
+          py={10}
+          px={6}
+          position="relative"
+          overflow="hidden"
+          height={"100%"}
+          width={"100%"}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <WarningIcon boxSize={"50px"} color={"red.500"} zIndex="100" />
+          <Heading as="h2" size="xl" mt={6} mb={2} zIndex="100">
+            {t("contactMe.error.title")}
+          </Heading>
+          <Text color={"gray.500"} zIndex="100">
+            {t("contactMe.error.description")}
+          </Text>
+        </Box>
+      );
+    }
+    if (emailSent) {
+      return (
+        <Box
+          textAlign="center"
+          py={10}
+          px={6}
+          position="relative"
+          overflow="hidden"
+          height={"100%"}
+          width={"100%"}
+          display="flex"
+          flexDirection={"column"}
+          justifyContent={"center"}
+          alignItems={"center"}
+        >
+          <CheckCircleIcon boxSize={"50px"} color={"green.500"} zIndex="100" />
+          <Heading as="h2" size="xl" mt={6} mb={2} zIndex="100">
+            {t("contactMe.emailSent.title")}
+          </Heading>
+          <Text color={"gray.500"} zIndex="100">
+            {t("contactMe.emailSent.description")}
+          </Text>
+          <SvgRocket />
+        </Box>
+      );
+    }
+    return (
+      <>
+        <form onSubmit={onSubmit} style={{ width: "100%" }}>
+          <VStack spacing={3}>
+            <FormControl isRequired>
+              <FormLabel> {t("contactMe.form.name.title")}</FormLabel>
+              <InputGroup>
+                <InputLeftElement>
+                  <BsPerson />
+                </InputLeftElement>
+                <Input
+                  type="text"
+                  placeholder={t("contactMe.form.name.placeholder")}
+                  {...register("name")}
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>{t("contactMe.form.email.title")}</FormLabel>
+              <InputGroup>
+                <InputLeftElement>
+                  <MdOutlineEmail />
+                </InputLeftElement>
+                <Input
+                  type="email"
+                  placeholder={t("contactMe.form.email.placeholder")}
+                  {...register("email")}
+                />
+              </InputGroup>
+            </FormControl>
+            <FormControl isRequired>
+              <FormLabel>{t("contactMe.form.message.title")}</FormLabel>
+
+              <Textarea
+                placeholder={t("contactMe.form.message.placeholder")}
+                rows={6}
+                resize="none"
+                {...register("message")}
+              />
+            </FormControl>
+            <Button
+              colorScheme="blackAlpha"
+              bg="black"
+              color="white"
+              _hover={{
+                bg: "gray.700"
+              }}
+              isFullWidth
+              type="submit"
+              isLoading={sendEmailLoading}
+            >
+              {t("contactMe.form.send")}
+            </Button>
+          </VStack>
+        </form>
+        <Box textAlign={"right"} mt="1rem">
+          <Link
+            display="inline-flex"
+            alignItems={"center"}
+            textAlign={"right"}
+            onClick={() => {
+              navigator.clipboard.writeText("jesusliang96@gmail.com");
+              toast({
+                title: t("alerts.copiedToClipboard"),
+                status: "success",
+                isClosable: true
+              });
+            }}
+          >
+            <MdEmail size="20px" />
+            jesusliang96@gmail.com
+          </Link>
+        </Box>
+      </>
+    );
+  };
   return (
     <Box color="white" padding={"3rem 0"} id="contact">
       <Text fontSize={"5xl"} fontWeight={"semibold"} textAlign="center">
@@ -65,108 +191,7 @@ const ContactMe: React.FC<Props> = (props) => {
           display="grid"
           placeItems={"center"}
         >
-          {emailSent ? (
-            <Box
-              textAlign="center"
-              py={10}
-              px={6}
-              position="relative"
-              overflow="hidden"
-              height={"100%"}
-              width={"100%"}
-              display="flex"
-              flexDirection={"column"}
-              justifyContent={"center"}
-              alignItems={"center"}
-            >
-              <CheckCircleIcon
-                boxSize={"50px"}
-                color={"green.500"}
-                zIndex="100"
-              />
-              <Heading as="h2" size="xl" mt={6} mb={2} zIndex="100">
-                {t("contactMe.emailSent.title")}
-              </Heading>
-              <Text color={"gray.500"} zIndex="100">
-                {t("contactMe.emailSent.description")}
-              </Text>
-              <SvgRocket />
-            </Box>
-          ) : (
-            <>
-              <form onSubmit={onSubmit} style={{ width: "100%" }}>
-                <VStack spacing={3}>
-                  <FormControl isRequired>
-                    <FormLabel> {t("contactMe.form.name.title")}</FormLabel>
-                    <InputGroup>
-                      <InputLeftElement>
-                        <BsPerson />
-                      </InputLeftElement>
-                      <Input
-                        type="text"
-                        placeholder={t("contactMe.form.name.placeholder")}
-                        {...register("name")}
-                      />
-                    </InputGroup>
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel>{t("contactMe.form.email.title")}</FormLabel>
-                    <InputGroup>
-                      <InputLeftElement>
-                        <MdOutlineEmail />
-                      </InputLeftElement>
-                      <Input
-                        type="email"
-                        placeholder={t("contactMe.form.email.placeholder")}
-                        {...register("email")}
-                      />
-                    </InputGroup>
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel>{t("contactMe.form.message.title")}</FormLabel>
-
-                    <Textarea
-                      placeholder={t("contactMe.form.message.placeholder")}
-                      rows={6}
-                      resize="none"
-                      {...register("message")}
-                    />
-                  </FormControl>
-                  <Button
-                    colorScheme="blackAlpha"
-                    bg="black"
-                    color="white"
-                    _hover={{
-                      bg: "gray.700"
-                    }}
-                    isFullWidth
-                    type="submit"
-                    isLoading={sendEmailLoading}
-                  >
-                    {t("contactMe.form.send")}
-                  </Button>
-                </VStack>
-              </form>
-              <Box textAlign={"right"} mt="1rem">
-                <Link
-                  display="inline-flex"
-                  alignItems={"center"}
-                  textAlign={"right"}
-                  onClick={() => {
-                    navigator.clipboard.writeText("jesusliang96@gmail.com");
-                    toast({
-                      title: t("alerts.copiedToClipboard"),
-                      status: "success",
-                      isClosable: true
-                    });
-                  }}
-                >
-                  <MdEmail size="20px" />
-                  jesusliang96@gmail.com
-                </Link>
-              </Box>
-            </>
-          )}
+          {renderEmailBox()}
         </Box>
       </Box>
     </Box>
--- pages/api/mailer.ts
+++ pages/api/mailer.ts
@@ -26,22 +26,26 @@ export default async function handler(
     if (!req.body.message) {
       return res.status(422).json({ error: "required", key: "message" });
     }
-    const nodemailer = require("nodemailer");
-    const transporter = nodemailer.createTransport({
-      host: "smtp.hostinger.com",
-      port: 465,
-      secure: true,
-      auth: {
-        user: "no-reply@jesusliang.com",
-        pass: "S{5&VMRyt{Pe'2@W"
-      }
-    });
-    const info = await transporter.sendMail({
-      from: "no-reply@jesusliang.com",
-      to: "jesusliang96@gmail.com, contact@jesusliang.com",
-      subject: "portfolio contact email",
-      text: `from: ${req.body.name} - ${req.body.email} \n ${req.body.message}`
-    });
-    res.status(200).send(true);
+    try {
+      const nodemailer = require("nodemailer");
+      const transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "no-reply@jesusliang.com",
+          pass: "S{5&VMRyt{Pe'2@W"
+        }
+      });
+      const info = await transporter.sendMail({
+        from: "no-reply@jesusliang.com",
+        to: "jesusliang96@gmail.com, contact@jesusliang.com",
+        subject: "portfolio contact email",
+        text: `from: ${req.body.name} - ${req.body.email} \n ${req.body.message}`
+      });
+      res.status(200).send(true);
+    } catch (error) {
+      res.status(500).send(false);
+    }
   }
 }
