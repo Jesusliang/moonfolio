1f42f765830cedfa56fe2bd7303d18e134dfa0cb
Jesus Liang <jesusliang96@gmail.com>
2022-06-30 14:42:11 +0200

    debugging google tag

--- pages/_document.js
+++ pages/_document.js
@@ -6,21 +6,19 @@ export default function Document () {
   return (
     <Html>
       <Head>
-        <Script
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5CD36VV');`
-        }}></Script>
+      <Script strategy="afterInteractive" dangerouslySetInnerHTML={{
+        __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5CD36VV');`
+      }}></Script>
       </Head>
       <body>
-        <noscript dangerouslySetInnerHTML={{
-          __html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CD36VV" height="0" width="0" style="display:none;visibility:hidden"></iframe>'
-        }}>
-        </noscript>
+      <noscript dangerouslySetInnerHTML={{
+        __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CD36VV"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+      }}></noscript>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <Main />
         <NextScript />
