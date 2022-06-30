34ca9aa956e79b9008414bbf6d7a6bc5b6356d14
Jesus Liang <jesusliang96@gmail.com>
2022-06-30 14:19:36 +0200

    added google tag

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,27 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 import { ColorModeScript } from '@chakra-ui/react'
 import { theme } from './_app'
+import Script from 'next/script'
 export default function Document () {
   return (
     <Html>
-      <Head />
+      <Head>
+        <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5CD36VV');`
+        }} />
+      </Head>
       <body>
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5CD36VV" height="0" width="0" style="display:none;visibility:hidden"></iframe>
+        </noscript>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-
         <Main />
         <NextScript />
       </body>
